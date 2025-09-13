13384a6ad8665f0b5af772bc02c3340a8fc45f93
sega <sshunter2013@gmail.com>
2025-09-14 00:55:02 +0700

    memperbaiki config webpack common

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: '/Notes-RESTfull/',
+    publicPath: "/Notes-RESTfull/", // penting untuk GitHub Pages
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "img/[name][ext]",
-        },
+        generator: { filename: "img/[name][ext]" },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][ext]",
-        },
+        generator: { filename: "fonts/[name][ext]" },
       },
     ],
   },
